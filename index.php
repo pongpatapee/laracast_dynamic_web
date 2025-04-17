ad3b5258538c8c0b539d3e85d809a6e8a4255925
pongpatapee <pongpatapee1021@gmail.com>
2025-04-17 18:05:51 -0400

    feat: pdo connection to mysql db

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php
 
 require("functions.php");
-require("router.php");
+// require("router.php");
+
+
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
