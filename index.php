0b4f52431db5b10bfda0d034dae638fea0c28080
pongpatapee <pongpatapee1021@gmail.com>
2025-04-17 18:28:35 -0400

    refactor: database class to take config

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ require("Database.php");
 // require("router.php");
 
 
-$db = new Database();
+$config = require("config.php");
 
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$db = new Database($config['database']);
+
+$posts = $db->query("select * from posts")->fetchAll();
 
 dd($posts);
