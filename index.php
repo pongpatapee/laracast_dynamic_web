bca1b22775ccac1b51d7225d86c047c254c6fcef
pongpatapee <pongpatapee1021@gmail.com>
2025-04-17 23:58:29 -0400

    feat: added note/notes view and controller

--- index.php
+++ index.php
@@ -2,13 +2,5 @@
 
 require("functions.php");
 require("Database.php");
-// require("router.php");
+require("router.php");
 
-
-$config = require("config.php");
-
-$db = new Database($config['database']);
-
-$posts = $db->query("select * from posts")->fetchAll();
-
-dd($posts);
