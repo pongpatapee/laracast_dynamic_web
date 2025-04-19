9111fa56451f9ddee32504506a8cf269396f178e
pongpatapee <pongpatapee1021@gmail.com>
2025-04-18 20:49:44 -0400

    refactor: fetch/404 and authorization

--- Database.php
+++ Database.php
@@ -4,6 +4,7 @@ class Database
 {
 
     public $connection;
+    public $statement;
 
     public function __construct($config, $username = 'root', $password = '')
     {
@@ -17,8 +18,29 @@ class Database
 
     public function query($query, $params = [])
     {
-        $statement = $this->connection->prepare($query);
-        $statement->execute($params);
-        return $statement;
+        $this->statement = $this->connection->prepare($query);
+        $this->statement->execute($params);
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+
+        if (! $result) {
+            abort();
+        }
+
+        return $result;
     }
 }
--- controllers/note.php
+++ controllers/note.php
@@ -5,16 +5,10 @@ $db = new Database($config['database']);
 
 $heading = "Note";
 
-$note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->fetch();
-
-if (!$note) {
-    abort();
-}
+$note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->findOrFail();
 
 $currentUserId = 1;
 
-if ($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] === $currentUserId);
 
 require("views/note.view.php");
